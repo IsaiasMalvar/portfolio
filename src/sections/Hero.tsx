a4b262b28faa5776644050178174c65819c53f6b
IsaMG93 <isamalk35@gmail.com>
2024-11-07 22:21:21 +0000

    Add stars and sparks to orbit element

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,11 +5,12 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import { HeroOrbit } from "@/components/HeroOrbit";
 import StartIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-38 lg:py-45 relative z-0 overflow-x-clip">
-      <div className="container">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom, transparent, black_10%,black_70%,transparent)">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{
@@ -21,41 +22,65 @@ export const HeroSection = () => {
         <div className=" size-[1020px] hero-ring"></div>
         <div className=" size-[1220px] hero-ring"></div>
         <HeroOrbit size={800} rotation={-72}>
-          <StartIcon className="size-28 text-emerald-300" />
+          <StartIcon className="size-28 text-cyan-300" />
         </HeroOrbit>
+        <HeroOrbit size={640} rotation={34}>
+          <StartIcon className="size-8 text-cyan-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StartIcon className="size-12 text-cyan-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-cyan-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-8 text-cyan-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-cyan-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-cyan-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={85}>
+          <div className="size-3 rounded-full bg-cyan-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-2 rounded-full bg-cyan-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-2 rounded-full bg-cyan-300/20"></div>
+        </HeroOrbit>
+      </div>
 
-        <div className="flex flex-col items-center ">
-          <Image
-            src={manSitting}
-            className="size-[250px] md:size-[350px]"
-            alt="Man sitting with laptop"
-          />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg mt-2">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">
-              Available for new projects
-            </div>
-          </div>
+      <div className="flex flex-col items-center ">
+        <Image
+          src={manSitting}
+          className="size-[250px] md:size-[350px]"
+          alt="Man sitting with laptop"
+        />
+        <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg mt-2">
+          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="text-sm font-medium">Available for new projects</div>
         </div>
-        <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl">
-            Crafting Scalable Solutions from Frontend to Backend
-          </h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">
-            My skills are focused in providing self-reliant, trustworthy
-            sofware.
-          </p>
-        </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-            <span className="font-medium tracking-wide">Explore my work</span>
-            <ArrowDown className="size-4" />
-          </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-            <span>🧑‍💻</span>
-            <span className="font-medium">Let's Connect</span>
-          </button>
-        </div>
+      </div>
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl">
+          Crafting Scalable Solutions from Frontend to Backend
+        </h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">
+          My skills are focused in providing self-reliant, trustworthy sofware.
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-medium tracking-wide">Explore my work</span>
+          <ArrowDown className="size-4" />
+        </button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <span>🧑‍💻</span>
+          <span className="font-medium">Let's Connect</span>
+        </button>
       </div>
     </div>
   );
